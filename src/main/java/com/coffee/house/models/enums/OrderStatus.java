151a8c67ed59262eb7019f867eb39e2d3d5e2868
= <=>
2024-01-15 15:27:46 +0700

    project models

--- src/main/java/com/coffee/house/models/enums/OrderStatus.java
+++ src/main/java/com/coffee/house/models/enums/OrderStatus.java
@@ -1,37 +1,50 @@
 package com.coffee.house.models.enums;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum OrderStatus {
 	SHIPPED, DELIVERED, PENDING, PROCESSING, CANCELLED;
-	
+
 	public String getName() {
 		String status;
-		
+
 		switch (this) {
-		case PENDING:{
-			status = "Chờ xử lý";		
-			break;
+			case PENDING: {
+				status = "Chờ xử lý";
+				break;
+			}
+			case PROCESSING: {
+				status = "Đang xử lý";
+				break;
+			}
+			case SHIPPED: {
+				status = "Đang vận chuyển";
+				break;
+			}
+			case DELIVERED: {
+				status = "Đã giao";
+				break;
+			}
+			case CANCELLED: {
+				status = "Đã hủy";
+				break;
+			}
+			default:
+				status = "erro";
+				break;
 		}
-		case PROCESSING:{
-			status = "Đang xử lý";
-			break;
-		}
-		case SHIPPED:{
-			status = "Đang vận chuyển";		
-			break;
-		}
-		case DELIVERED:{
-			status = "Đã giao";		
-			break;
-		}
-		case CANCELLED:{
-			status = "Đã hủy";		
-			break;
-		}
-		default:
-			status = "erro";
-			break;
-		}				
 		return status;
 	}
-	
+
+	public static Map<String, String> getListStateTranstionOrder() {
+		// state transtion : PENDING -> PROCESSING -> SHIPPED -> DELIVERED
+		Map<String, String> stateTranstions = new HashMap<>();
+
+		stateTranstions.put(OrderStatus.PENDING.getName(), OrderStatus.PENDING.name());
+		stateTranstions.put(OrderStatus.PROCESSING.getName(), OrderStatus.PROCESSING.name());
+		stateTranstions.put(OrderStatus.SHIPPED.getName(), OrderStatus.SHIPPED.name());
+		stateTranstions.put(OrderStatus.DELIVERED.getName(), OrderStatus.DELIVERED.name());
+		return stateTranstions;
+	}
 }
--- src/main/java/com/coffee/house/models/enums/PaymentStatus.java
+++ src/main/java/com/coffee/house/models/enums/PaymentStatus.java
@@ -1,29 +1,40 @@
 package com.coffee.house.models.enums;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum PaymentStatus {
 	PENDING, SUCCESS, CANCELLED;
-	
+
 	public String getName() {
 		String status;
-		
+
 		switch (this) {
-		case SUCCESS:{
-			status = "Thành công";
-			break;
+			case SUCCESS: {
+				status = "Thành công";
+				break;
+			}
+			case PENDING: {
+				status = "Chờ thanh toán";
+				break;
+			}
+			case CANCELLED: {
+				status = "Đã hủy";
+				break;
+			}
+			default:
+				status = "erro";
+				break;
 		}
-		case PENDING:{
-			status = "Chờ thanh toán";		
-			break;
-		}
-		case CANCELLED:{
-			status = "Đã hủy";		
-			break;
-		}
-		default:
-			status = "erro";
-			break;
-		}				
 		return status;
 	}
-	
+
+	public static Map<String, String> getListStateTranstionPayment() {
+		// state transtion : PENDING -> SUCCESS
+		Map<String, String> stateTranstions = new HashMap<>();
+
+		stateTranstions.put(PaymentStatus.PENDING.getName(), PaymentStatus.PENDING.name());
+		stateTranstions.put(PaymentStatus.SUCCESS.getName(), PaymentStatus.SUCCESS.name());
+		return stateTranstions;
+	}
 }
